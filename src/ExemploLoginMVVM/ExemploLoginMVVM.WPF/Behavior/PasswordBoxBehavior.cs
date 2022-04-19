4df639a760752e0afb81dbb3fa35237a6fd45248
Incógnito <incognitosenpai860@gmail.com>
2022-04-19 08:59:13 -0300

    New: Implementação da classe PasswordBoxBehavior

---
+++ src/ExemploLoginMVVM/ExemploLoginMVVM.WPF/Behavior/PasswordBoxBehavior.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Linq;
+using System.Security;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ExemploLoginMVVM.WPF.Behavior
+{
+    public class PasswordBoxBehavior : Behavior<PasswordBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.LostFocus += OnComparePasswordLostFocus;
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.LostFocus -= OnComparePasswordLostFocus;
+            base.OnDetaching();
+        }
+
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(SecureString), typeof(PasswordBoxBehavior), new FrameworkPropertyMetadata(null) { BindsTwoWayByDefault = true });
+
+
+        public SecureString Password
+        {
+            get { return (SecureString)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        private static void OnComparePasswordLostFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox pswdBox = sender as PasswordBox;
+            PasswordBoxBehavior behavior = Interaction.GetBehaviors(pswdBox).OfType<PasswordBoxBehavior>().FirstOrDefault();
+            if (behavior != null)
+            {
+                behavior.Password = pswdBox.SecurePassword;
+            }
+        }
+    }
+}
