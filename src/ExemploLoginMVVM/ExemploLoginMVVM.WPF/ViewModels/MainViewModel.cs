b1966c71f7798eb8ac3a878e19742848e323c8fb
Incógnito <incognitosenpai860@gmail.com>
2022-04-20 17:21:40 -0300

    New: Confecção da MainViewModel e ligação de dados.

---
+++ src/ExemploLoginMVVM/ExemploLoginMVVM.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploLoginMVVM.WPF.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        public MainViewModel()
+        {
+            LoginCommand = new DelegateCommand(Login);
+        }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public DelegateCommand LoginCommand { get; set; }
+
+        public void Login()
+        {
+
+        }
+    }
+}
